d8444598439484a84cdc14a64621fbc0abbfe0c6
Conor Grocock <conor@grocock.email>
2019-10-01 21:00:26 +0100

    Added planning

--- src/currency/CurrencyConverter.java
+++ src/currency/CurrencyConverter.java
@@ -6,6 +6,14 @@ public class CurrencyConverter {
 
     // STUB TO FAIL THE TEST
     public static CurrencyAmount convert(CurrencyAmount source, Currency target) {
+		// URL: https://api.exchangeratesapi.io/latest?symbols=USD&base=GBP
+		// source: source.getCurrencyKey().getCurrencyCode()
+		// target: target.getCurrencyCode()
+
+		// URL url = new URL("http://example.com");
+		// HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		// con.setRequestMethod("GET");
+
         return new CurrencyAmount(-100 * source.getUnitAmount(), target);
     }
 
